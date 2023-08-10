9e8648c1a262d4515b88c8376f6c64b47b6ae6c3
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-10 22:16:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
-}
-
-window.decimalToBinary = decimalToBinary;
+    function decimalToBinary(decimal) {
+      return decimal.toString(2);
+    }
+     
+    console.log(decimalToBinary(7));
+    console.log(decimalToBinary(10)); 
+    console.log(decimalToBinary(33)); 
